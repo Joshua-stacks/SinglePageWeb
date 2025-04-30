60d9d0f99daf7b55550c40fe324fe7bcdc029ab0
Joshua-stacks <vasquez.joshua1115@gmail.com>
2025-04-30 15:47:13 -0400

    all basic links

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-
 import "./App.css";
 
 function App() {
-
-
   return (
     <>
       <div>
@@ -26,6 +23,39 @@ function App() {
         <ul>
           <li>
             <a href="https://form.jotform.com/250455957815265">KYC Form</a>
+          </li>
+          <li>
+            <a href="https://richemontb2c.b2clogin.com/richemontb2c.onmicrosoft.com/oauth2/v2.0/authorize?client_id=6e3015fa-71c5-4c93-aa6d-7eeaf6bf014a&p=B2C_1A_RIC-COMBINEDSIGNIN&redirect_uri=https%3A%2F%2Fbooster-richemont.my.salesforce.com%2Fservices%2Fauthcallback%2FAzureB2C&response_type=code&scope=openid+profile+email+offline_access+https%3A%2F%2Frichemontb2c.onmicrosoft.com%2F8b6e634f-d1c2-4cd9-b280-091343351d32%2FAccess&state=CAAAAZaIR7BcMDAwMDAwMDAwMDAwMDAwAAAA_q_eEFyIzkUJnnv7U-VqBzBIoSZ3jZgJ5QfWDFK_bAampsHCYL4Monb3wOrJNMNd8bkANBTNG3UnsgCpmtFsFWlFlKLZHJ8FWG9wKoMqRhssPxVbdQN48ccymZkyKeqWxSkFXRrZIqxP9h1MmvE-Em9VofNNugJ4h5XcRHLGPg5ZfKNKGVTfZ7NH6jEzaM4SRpZFJmYgrcUe0OzZlA8qHJMj0V3eCZsblrm1Pc0dBw-B6QZmTaYFw7C-UoxPFOZHaUcolOGWx9ERKPPDoMioYhY%3D">Richemont Warranty (Cartier, IWC, Panarai, Piaget, JLC, Baume & Mercier)</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h2>Inquiry + purchases</h2>
+        <ul>
+          <li>
+            <a href="https://form.jotform.com/213426593347056">
+              Online Payment Request Form
+            </a>
+          </li>
+          <li>
+            <a href="https://form.jotform.com/91535549597271">
+              Sales Inquiry/Order Form
+            </a>
+          </li>
+          <li>
+            <a href="https://form.jotform.com/91545011606247">
+              Strap Inquiry/Order Form
+            </a>
+          </li>
+          <li>
+            <a href="https://form.jotform.com/222865958155065">
+              Rolex Request Form
+            </a>
+          </li>
+          <li>
+            <a href="https://form.jotform.com/232905181275254">
+              Engagement Ring Quote Reques
+            </a>
           </li>
         </ul>
       </div>
